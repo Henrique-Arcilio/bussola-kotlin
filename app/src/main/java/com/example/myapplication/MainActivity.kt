ea8185622cc5f1f3543cd4ba676e76bb091b5e30
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-05-01 16:36:47 -0300

    feat: configurando imagem e setup basico dos sensores

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var sensorManager: SensorManager ?= null
+    var sensor : Sensor ?= null
+
+    lateinit var bussolaImage : ImageView
+    lateinit var texto : TextView
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        print(p1)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        Log.d(TAG, "onSensorChanged: " + p0?.values[0])
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause();
+        sensorManager?.unregisterListener(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        bussolaImage = findViewById<ImageView>(R.id.bussulaImagem)
+        texto = findViewById<TextView>(R.id.texto)
     }
 }
