12a48c724de8063e6a8183d3df9d8d12a149b0a7
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-05-04 10:03:00 -0300

    refactor: melhoria na estrutra e utilização da memório
    - Muda alguns nomes de variáveis para ficar mais semântico
    - Corrige a forma de carregar os arrays utilizando arrayCopy
    - Adiciona mensagem para quando não encontrar os sensores

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,73 +6,67 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    var sensorManager: SensorManager ?= null
-    var accelerometerSensor : Sensor ?= null
-    var magneticSensor : Sensor ?= null;
-    var hasGravidade = false
-    var hasNorteMag = false
-    var vetorGravidade = FloatArray(3)
-    var vetorNorteMagnetico = FloatArray(3)
+    private var sensorManager: SensorManager ?= null
+    private var sensorAcelerometro : Sensor ?= null
+    private var sensorMagnetico : Sensor ?= null;
 
+    private var hasGravidade = false
+    private var hasNorteMag = false
+
+    private val vetorGravidade = FloatArray(3)
+    private val vetorNorteMagnetico = FloatArray(3)
+
+    private val matrizRotacao = FloatArray(9)
+
+    private val angulosOrientacao = FloatArray(3)
 
     lateinit var bussolaImage : ImageView
-    lateinit var texto : TextView
+    lateinit var tvRotacao : TextView
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensorAcelerometro = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorMagnetico = sensorManager?.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        bussolaImage = findViewById<ImageView>(R.id.bussulaImagem)
+        tvRotacao = findViewById<TextView>(R.id.texto)
+
+        if (sensorAcelerometro == null) {
+            tvRotacao.text = "Sem acelerômetro"
+        }
+
+        if (sensorMagnetico == null) {
+            tvRotacao.text = "Sem magnetômetro"
+        }
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
 
-        if (event == null) return
-
-        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            vetorGravidade = event.values.clone()
-            hasGravidade = true
-        }
-
-        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-            vetorNorteMagnetico = event.values.clone()
-            hasNorteMag = true
-        }
-
-        if(hasGravidade && hasNorteMag){
-            var rotacao = FloatArray(9);
-            var orientacao = FloatArray(3)
-
-            val funcionou =  SensorManager.getRotationMatrix(rotacao, null, vetorGravidade, vetorNorteMagnetico)
-
-            if(funcionou){
-                SensorManager.getOrientation(rotacao, orientacao)
-
-                var azimuth = Math.toDegrees(orientacao[0].toDouble()).toFloat()
-
-                azimuth = (azimuth + 360) % 360
-
-                bussolaImage.rotation = -azimuth
-                texto.text = "Rotação: %d".format(azimuth.toInt())
-            }
-
-        }
-
-
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 
     override fun onResume() {
         super.onResume()
 
-        if(magneticSensor != null){
-            sensorManager?.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_GAME)
+        if(sensorMagnetico != null){
+            sensorManager?.registerListener(this, sensorMagnetico, SensorManager.SENSOR_DELAY_GAME)
 
         }
-        if(accelerometerSensor != null){
-            sensorManager?.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME)
+        if(sensorAcelerometro != null){
+            sensorManager?.registerListener(this, sensorAcelerometro, SensorManager.SENSOR_DELAY_GAME)
         }
     }
 
@@ -81,13 +75,34 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager?.unregisterListener(this)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        accelerometerSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        magneticSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        bussolaImage = findViewById<ImageView>(R.id.bussulaImagem)
-        texto = findViewById<TextView>(R.id.texto)
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if (event == null) return
+
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, vetorGravidade, 0, 3)
+            hasGravidade = true
+        }
+
+        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, vetorNorteMagnetico, 0, 3)
+            hasNorteMag = true
+        }
+
+        if(hasGravidade && hasNorteMag){
+
+            val funcionou =  SensorManager.getRotationMatrix(matrizRotacao, null, vetorGravidade, vetorNorteMagnetico)
+
+            if(funcionou){
+                SensorManager.getOrientation(matrizRotacao, angulosOrientacao)
+
+                var azimuth = Math.toDegrees(angulosOrientacao[0].toDouble()).toFloat()
+
+                azimuth = (azimuth + 360) % 360
+
+                bussolaImage.rotation = -azimuth
+                tvRotacao.text = "Rotação: %d".format(azimuth.roundToInt())
+            }
+        }
     }
 }
